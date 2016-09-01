c757c24ff59977fa97d1af7a8e5c36ff9fb5620f
losandes <andes.collab@gmail.com>
2016-09-01 09:50:29 -0400

    removes view stuff from expressStartup

--- templates/projects/express-api/expressStartup.js
+++ templates/projects/express-api/expressStartup.js
@@ -6,27 +6,36 @@ module.exports.dependencies = [
     'bodyParser',
     'serve-static',
     'less',
-    'hbs',
-    'hbsBlocks',
     'favicon',
     'defaultCorsHandler'
 ];
-module.exports.factory = function (app, path, cookieParser, bodyParser, serveStatic, less, hbs, extendHbs, favicon, cors) {
+module.exports.factory = function (app, path, cookieParser, bodyParser, serveStatic, less, favicon, cors) {
     'use strict';
 
     var before,
-        after;
+        after,
+        init;
+
+    init = function (applicationLifecycle, next) {
+        before();
+
+        if (typeof applicationLifecycle === 'function') {
+            applicationLifecycle(app);
+        }
+
+        after();
+
+        if (typeof next === 'function') {
+            next(app);
+        }
+
+        return app;
+    };
 
     before = function () {
-        // view engine setup
-        app.set('views', path.join(__dirname, 'views'));
-        app.set('view engine', 'hbs');
-        hbs.registerPartials(__dirname + '/views/templates');
-        extendHbs(hbs);
-
         app.use(cors);
         app.use(bodyParser.json());
-        app.use(bodyParser.urlencoded({ extended: true }));
+        app.use(bodyParser.urlencoded({ extended: false }));
         app.use(cookieParser());
         app.use(less(path.join(__dirname, 'public')));
         app.use(serveStatic(path.join(__dirname, 'public')));
@@ -36,7 +45,9 @@ module.exports.factory = function (app, path, cookieParser, bodyParser, serveSta
     after = function () {
         // make 404's a greedy index route for the SPA
         app.use(function (req, res, next) {
-            res.render('index', { title: '<%= projectName %>' });
+            res.status(404).send({
+                status: 404
+            });
         });
 
         // error handlers
@@ -46,33 +57,19 @@ module.exports.factory = function (app, path, cookieParser, bodyParser, serveSta
         if (app.get('env') === 'development') {
             app.use(function (err, req, res, next) {
                 res.status(err.status || 500);
-                res.render('error', { title: 'error', message: err.message, error: err });
+                res.send({ title: 'error', message: err.message, error: err });
             });
         } else {
             // production error handler
             // no stacktraces leaked to user
             app.use(function (err, req, res, next) {
                 res.status(err.status || 500);
-                res.render('error', { title: 'error', message: err.message, error: {} });
+                res.send({ title: 'error', message: err.message, error: {} });
             });
         }
     };
 
     return {
-        init: function (applicationLifecycle, next) {
-            before();
-
-            if (typeof applicationLifecycle === 'function') {
-                applicationLifecycle(app);
-            }
-
-            after();
-
-            if (typeof next === 'function') {
-                next(app);
-            }
-
-            return app;
-        }
+        init: init
     };
 };
