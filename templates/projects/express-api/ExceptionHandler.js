96536a777a2f917c865b2eda06cc5d0916b002f1
losandes <andes.collab@gmail.com>
2016-09-01 10:28:54 -0400

    better exception handler

--- templates/projects/express-api/ExceptionHandler.js
+++ templates/projects/express-api/ExceptionHandler.js
@@ -1,80 +1,82 @@
 module.exports.name = 'ExceptionHandler';
-module.exports.dependencies = [];
-module.exports.factory = function (onError) {
+module.exports.singleton = true;
+module.exports.dependencies = ['hilary::is'];
+module.exports.factory = function (is) {
     'use strict';
 
-    var self = {
+    return function (onError) {
+        var self = {
             makeException: undefined,
-            argumentException: undefined,
-            throwArgumentException: undefined,
-            notImplementedException: undefined,
-            throwNotImplementedException: undefined,
-            throwException: undefined,
-            throw: undefined
-        },
-        makeException;
+            makeArgumentException: undefined,
+            throw: undefined,
+            throwArgumentException: undefined
+        };
 
-    onError = (typeof onError === 'function') ? onError : function (exception) {
-        console.error(exception);
-        throw exception;
+        onError = (typeof onError === 'function') ? onError : function (exception) {
+            console.error(exception);
+            throw exception;
+        };
+
+        self.makeException = function (messageOrError, data) {
+            var err,
+                isDataOnMessage,
+                isDataArg,
+                prop;
+
+            if (is.object(messageOrError)) {
+                err = messageOrError;
+            } else if (is.string(messageOrError)) {
+                err = new Error(messageOrError);
+            } else {
+                err = new Error();
+            }
+
+            isDataOnMessage = is.defined(err.data);
+            isDataArg = is.defined(data);
+
+            if (isDataOnMessage && isDataArg) {
+                for (prop in data) {
+                    if (data.hasOwnProperty(prop)) {
+                        err.data[prop] = data[prop];
+                    }
+                }
+            } else if (isDataArg) {
+                err.data = data;
+            }
+
+            err.type = err.type || 'exception';
+
+            return err;
+        };
+
+        self.makeArgumentException = function (error, argument, data) {
+            var err;
+
+            if (is.object(error)) {
+                error.type = 'argumentException';
+                return self.makeException(error, data);
+            } else {
+                err = new Error(error);
+                err.type = 'argumentException';
+
+                return self.makeException(err, data);
+            }
+        };
+
+        self.throw = function (exception, data) {
+            onError(self.makeException(exception, data));
+        };
+
+        self.throwArgumentException = function (error, argument, data) {
+            onError(self.makeArgumentException(error, argument, data));
+        };
+
+        self.throwWithInnerException = function (exception, innerException, data) {
+            var exc = self.makeException(exception, data);
+            exc.innerException = self.makeException(innerException);
+            onError(exc);
+        };
+
+        return self;
     };
-
-    makeException = function (name, message, data) {
-        var msg,
-            err;
-
-        if (typeof name === 'object' && typeof name.message === 'string') {
-            // The name argument probably received an Error object
-            msg = name.message;
-            err = name;
-        } else {
-            msg = typeof message === 'string' ? message : name;
-            err = new Error(msg);
-        }
-
-        err.message = msg;
-
-        if (name !== msg) {
-            err.name = name;
-        }
-
-        if (data) {
-            err.data = data;
-        }
-
-        return err;
-    };
-
-    self.makeException = makeException;
-
-    self.argumentException = function (message, argument, data) {
-        var msg = typeof argument === 'undefined' ? message : message + ' (argument: ' + argument + ')';
-        return makeException('ArgumentException', msg, data);
-    };
-
-    self.throwArgumentException = function (message, argument, data) {
-        self.throw(self.argumentException(message, argument, data));
-    };
-
-    self.notImplementedException = function (message, data) {
-        return makeException('NotImplementedException', message, data);
-    };
-
-    self.throwNotImplementedException = function (message, data) {
-        self.throw(self.notImplementedException(message, data));
-    };
-
-    self.throwException = function (exception) {
-        self.throw(exception);
-    };
-
-    self.throw = function (exception) {
-        if (typeof exception === 'string') {
-            onError(makeException(exception));
-        } else {
-            onError(exception);
-        }
-    };
-
-    return self;
 };
