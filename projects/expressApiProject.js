34eb5103641ac0e6a0fcf5f92d096bba180cadea
losandes <andes.collab@gmail.com>
2016-09-02 10:50:17 -0400

    puts layout back in api

--- projects/expressApiProject.js
+++ projects/expressApiProject.js
@@ -66,6 +66,7 @@ module.exports.init = function (path) {
                 $this.fs.copy(path.join(templatePath, '/public/css/docs-slate.less'), path.join(destinationPath, '/public/css/docs-slate.less'));
                 $this.fs.copyTpl(path.join(templatePath, '/public/scripts/docs.js'), path.join(destinationPath, '/public/scripts/docs.js'), $this.templatedata);
 
+                $this.fs.copy(path.join(templatePath, '/views/layout.hbs'), path.join(destinationPath, '/views/layout.hbs'));
                 $this.fs.copy(path.join(templatePath, '/views/docs.hbs'), path.join(destinationPath, '/views/docs.hbs'));
 
                 $this.on('end', function () {
